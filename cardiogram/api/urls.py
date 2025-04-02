60170fab6b47c8dc98113671cc647e8a714f7dd7
BruhMano <vanusha747@gmail.com>
2025-04-03 00:16:12 +0400

    исправлено отсутствие сырой строки в роутере

--- cardiogram/api/urls.py
+++ cardiogram/api/urls.py
@@ -6,10 +6,10 @@ from api.views import CardViewset, DeckViewset, UserProgressViewset, UserViewset
 router = routers.SimpleRouter()
 router.register(r'card', CardViewset)
 router.register(r'deck', DeckViewset)
-router.register('progress', UserProgressViewset, 'progress')
-router.register('user', UserViewset, 'user')
+router.register(r'progress', UserProgressViewset, 'progress')
+router.register(r'user', UserViewset, 'user')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path("auth/", views.obtain_auth_token),
+    path('auth/', views.obtain_auth_token),
 ]
